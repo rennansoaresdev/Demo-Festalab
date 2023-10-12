308df89bd10ed1837461db9ddc7a9d74d82514c1
Rennan Soares <rennansoares@icloud.com>
2023-10-12 19:42:20 -0300

    styling done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users or /users.json
   def index
     @users = if params[:search]
       User.where("name LIKE ? OR email LIKE ? OR phone LIKE ? OR cpf LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
@@ -10,26 +9,22 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1 or /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to user_url(@user), notice: "Usuario criado com sucesso." }
+        format.html { redirect_to user_url(@user), notice: "Usuario criado com sucesso."}
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +33,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -51,23 +45,20 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "Usuario apagado com sucesso." }
+      format.html { redirect_to users_url, notice: "Usuário apagado com sucesso." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email, :phone, :cpf)
     end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// Ask delete confirmation
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.addEventListener("click", function(event) {
+    if (event.target && event.target.id === "delete-user-btn") {
+      if (!confirm("Tem certeza de que deseja deletar este usuário?")) {
+        event.preventDefault();
+      }
+    }
+  });
+});
